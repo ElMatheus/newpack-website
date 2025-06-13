809456f8d966858afae2a4b95d7b13c3a7322676
ElMatheus <matheus.goncalves5@aluno.senai.br>
2025-06-13 18:02:32 -0300

    fix: remove unnecessary motion effect from header background

--- src/components/headerArticles/index.tsx
+++ src/components/headerArticles/index.tsx
@@ -1,17 +1,9 @@
 import { HeaderInfos } from "@/types/Header";
 import { AnimatedSection } from "../animations";
-import { motion } from "motion/react"
 
 export default function Header({ title, subtitle }: HeaderInfos) {
   return (
     <section className="relative py-12 bg-gradient-to-b from-[#00AFEF]/10 to-white">
-      <motion.div
-        className="absolute inset-0 bg-[url('/newpack.svg')] bg-no-repeat bg-center opacity-5 scale-150"
-        animate={{
-          scale: [1.5, 1.6, 1.5],
-          opacity: [0.05, 0.08, 0.05]
-        }}
-      ></motion.div>
       <div className="container mx-auto px-4 py-8">
         <AnimatedSection>
           <div className="flex flex-col items-center text-center">
